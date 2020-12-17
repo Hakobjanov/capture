1ea9774572de145660df02e51b8757e3849b4675
vahan akopjanian <vahan.bclub@gmail.com>
2020-12-17 22:34:45 +0200

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import GlobalStyles from "./components/GlobalStyles";
+import { Switch, Route } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import OurWork from "./pages/OurWork";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
