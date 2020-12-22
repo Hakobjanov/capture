b152f256beacce28eb21f0d8c23a7b83e5506ef3
vahan akopjanian <vahan.bclub@gmail.com>
2020-12-22 19:31:27 +0200

    add media queries

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -63,6 +63,26 @@ const StyledNav = styled.nav`
     font-size: 2rem;
     letter-spacing: 1px;
   }
+
+  @media (max-width: 1300px) {
+    flex-direction: column;
+    padding: 2rem 1rem;
+
+    #logo {
+      display: inline-block;
+      margin: 5rem;
+    }
+
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+
+      li {
+        padding: 0;
+      }
+    }
+  }
 `;
 
 export default Nav;
